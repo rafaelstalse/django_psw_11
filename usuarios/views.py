fa498fe3ccba4defcfe76848732d54c36dabc108
Rafael Paraiso Rossim <rafael@stalse.com>
2024-08-05 09:39:30 -0300

    commit com core e app usuários realizados

---
+++ usuarios/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha não conferem')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha deve possuir pelo menos 6 caracteres')
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe')
+            return redirect('/usuarios/cadastro')
+
+
+        user = User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+        return redirect('/usuarios/logar')
