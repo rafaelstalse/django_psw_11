d88a2394453de4e78b37c5ac1d5113fc30f1fa56
Rafael Paraiso Rossim <rafael@stalse.com>
2024-08-08 22:58:05 -0300

    add funcioalidade de sugestão de investimento

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
-    'empresarios'
+    'empresarios',
+    'investidores'
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('usuarios.urls')),
     path('usuarios/', include('usuarios.urls')),
     path('empresarios/', include('empresarios.urls')),
+    path('investidores/', include('investidores.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
